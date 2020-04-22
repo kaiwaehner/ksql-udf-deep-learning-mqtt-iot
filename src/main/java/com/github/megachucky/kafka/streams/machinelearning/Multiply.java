eda9777d6026c9b6f953b63694a0f0b5f1a9f0bd
kaiwaehner <megachucky@gmail.com>
2020-04-22 14:48:34 +0200

    Upgraded syntax to ksqlDB

--- src/main/java/com/github/megachucky/kafka/streams/machinelearning/Multiply.java
+++ src/main/java/com/github/megachucky/kafka/streams/machinelearning/Multiply.java
@@ -2,27 +2,28 @@ package com.github.megachucky.kafka.streams.machinelearning;
 
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
 
 @UdfDescription(name = "multiply", description = "multiplies 2 numbers")
 public class Multiply {
 
   @Udf(description = "multiply two non-nullable INTs.")
-  public long multiply(final int v1, final int v2) {
+  public long multiply(@UdfParameter("v1") final int v1, @UdfParameter("v2")  final int v2) {
     return v1 * v2;
   }
 
   @Udf(description = "multiply two non-nullable BIGINTs.")
-  public long multiply(final long v1, final long v2) {
+  public long multiply(@UdfParameter("v1") final long v1, @UdfParameter("v2")  final long v2) {
     return v1 * v2;
   }
 
   @Udf(description = "multiply two nullable BIGINTs. If either param is null, null is returned.")
-  public Long multiply(final Long v1, final Long v2) {
+  public Long multiply(@UdfParameter("v1") final Long v1, @UdfParameter("v2")  final Long v2) {
     return v1 == null || v2 == null ? null : v1 * v2;
   }
 
   @Udf(description = "multiply two non-nullable DOUBLEs.")
-  public double multiply(final double v1, double v2) {
+  public double multiply(@UdfParameter("v1") final double v1, @UdfParameter("v2")  double v2) {
     return v1 * v2;
   }
 }
